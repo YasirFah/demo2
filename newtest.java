063cc42d674960b083ccc990c9a972d1564040e3
YasirFah <yasiraltamimi178@gmail.com>
2023-10-06 17:46:20 +0300

    new branch

--- newtest.java
+++ newtest.java
@@ -2,7 +2,7 @@ public class newtest{
 public static void main(String[] args) {
     System.out.println("hey ");
     for (int i = 0; i < 5; i++) {
-        System.out.print(i+" ");
+        System.out.println(i);
     }
     System.out.println("now we in new branch");
 }
